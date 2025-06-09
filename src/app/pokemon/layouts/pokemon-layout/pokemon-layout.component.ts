984c96f8544ddef4dde9497f253d8402ee2a80ed
Edil <esh9912@gmail.com>
2025-06-08 23:19:26 -0400

    working on a footer

--- src/app/pokemon/layouts/pokemon-layout/pokemon-layout.component.ts
+++ src/app/pokemon/layouts/pokemon-layout/pokemon-layout.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopMenuComponent } from "../../components/top-menu/top-menu.component";
+import { FooterComponent } from "../../../shared/components/footer/footer.component";
 
 @Component({
   selector: 'pokemon-layout',
